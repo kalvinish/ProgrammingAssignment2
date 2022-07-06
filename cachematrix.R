64c31278a5b8d4d93ee628157c9dc72753e35133
kalvinish <70290451+kalvinish@users.noreply.github.com>
2022-07-06 17:49:40 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,10 @@
 ## cache its inverse. 
 
 ## This matrix object is really a list containing a function to:
-## 1. set the value of the vector
-## 2. get the value of the vector
-## 3. set the value of the mean
-## 4. get the value of the mean
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse matrix
+## 4. get the value of the inverse matrix
 
 makeCacheMatrix <- function(matrix = matrix()) {
     inverse <- NULL
